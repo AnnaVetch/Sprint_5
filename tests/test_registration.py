947eb78bef06239e8b500b75e6769d04193260f0
Anna Vetchinkina <vetchinkina.ann@yandex.ru>
2025-11-06 15:43:51 +0300

    fix wait time

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -11,14 +11,14 @@ def test_success_registration(driver):
 
     # Act
     driver.get(REG_URL)
-    WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(REG_BUTTON))
+    WebDriverWait(driver, 100).until(expected_conditions.visibility_of_element_located(REG_BUTTON))
     driver.find_element(*REG_NAME).send_keys(name)
     driver.find_element(*REG_EMAIL).send_keys(email)
     driver.find_element(*REG_PASS).send_keys(password)
     driver.find_element(*REG_BUTTON).click()
 
     # Assert
-    assert WebDriverWait(driver, 10).until(expected_conditions.url_contains(LOGIN_URL))
+    assert WebDriverWait(driver, 100).until(expected_conditions.url_contains(LOGIN_URL))
 
 
 
@@ -35,6 +35,6 @@ def test_error_short_password(driver):
     driver.find_element(*REG_BUTTON).click()
 
     # Assert
-    error_element = WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(REG_ERROR))
+    error_element = WebDriverWait(driver, 100).until(expected_conditions.visibility_of_element_located(REG_ERROR))
     assert error_element.is_displayed
     
