f981d9c25e585160bb2bed8d220d53e70b9f3c81
Anna Vetchinkina <vetchinkina.ann@yandex.ru>
2025-11-06 16:13:07 +0300

    tests are debugged

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -7,11 +7,16 @@ def test_constructor_section_buns_success(driver):
     # Переход из раздела "Конструктор" в раздел "Булки"
     # Arrange
     driver.get(HOST)
-    WebDriverWait(driver, 100).until(expected_conditions.url_to_be(HOST))
+    WebDriverWait(driver, 1000).until(expected_conditions.url_to_be(HOST))
 
     #Act
     driver.find_element(*SAUCES_TAB).click()
+
+    WebDriverWait(driver, 5).until(
+        lambda d: "tab_tab_type_current" in d.find_element(*SAUCES_TAB).get_attribute("class"))
     driver.find_element(*BUNS_TAB).click()
+    WebDriverWait(driver, 5).until(
+        lambda d: "tab_tab_type_current" in d.find_element(*BUNS_TAB).get_attribute("class"))
 
     #Assert
     assert "tab_tab_type_current" in driver.find_element(*BUNS_TAB).get_attribute("class")
@@ -20,10 +25,12 @@ def test_constructor_section_sauces_success(driver):
     # Переход из раздела "Конструктор" в раздел "Соусы"
     # Arrange
     driver.get(HOST)
-    WebDriverWait(driver, 100).until(expected_conditions.url_to_be(HOST))
+    WebDriverWait(driver, 1000).until(expected_conditions.url_to_be(HOST))
 
     # Act
     driver.find_element(*SAUCES_TAB).click()
+    WebDriverWait(driver, 5).until(
+        lambda d: "tab_tab_type_current" in d.find_element(*SAUCES_TAB).get_attribute("class"))
 
     #Assert
     assert "tab_tab_type_current" in driver.find_element(*SAUCES_TAB).get_attribute("class")
@@ -33,10 +40,13 @@ def test_constructor_section_fillings_success(driver):
     # Переход из раздела "Конструктор" в раздел "Начинки"
     # Arrange
     driver.get(HOST)
-    WebDriverWait(driver, 100).until(expected_conditions.url_to_be(HOST))
+    WebDriverWait(driver, 1000).until(expected_conditions.url_to_be(HOST))
 
     # Act
     driver.find_element(*FILLINGS_TAB).click()
+    WebDriverWait(driver, 5).until(
+        lambda d: "tab_tab_type_current" in d.find_element(*FILLINGS_TAB).get_attribute("class"))
+
 
     #Assert
     assert "tab_tab_type_current" in driver.find_element(*FILLINGS_TAB).get_attribute("class")
